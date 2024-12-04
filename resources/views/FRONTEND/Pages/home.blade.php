d52e4c3707777d8d48f2f7a564c60b1a55048231
hemantherenj <hemant.herenj@gmail.com>
2024-12-05 00:51:27 +0530

    h

--- resources/views/FRONTEND/Pages/home.blade.php
+++ resources/views/FRONTEND/Pages/home.blade.php
@@ -25,7 +25,7 @@
                     <img src="https://blissfulplans.com/wp-content/uploads/2020/06/christian-wedding.jpg" class="card-img-top"
                         alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title">Profile</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                             card's content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
